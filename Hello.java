f1266e0573553c27b4548b21c6d07bcc0410c050
moonjunha <moonjunha1@daum.net>
2020-11-29 22:16:30 +0900

    Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
-  System.out.println("not finish");
+  System.out.println("not finish"); 
+  System.out.println("finish");
  }
 }
